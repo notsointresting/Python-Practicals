8b6243b2babe88a4aae0985ff768be71c7591ce1
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2022-07-21 23:43:03 +0530

    Delete Case Study directory

--- Case Study/demo.sql
+++
@@ -1,5 +0,0 @@
-create database demo;
-use demo;
-create user 'sahiil' @'localhost'IDENTIFIED BY '3000';
-grant all on demo.*to 'sahiil'@'localhost';
-create table student(NAME varchar(20), SURNAME varchar(20), AGE int, PRN varchar(12));
